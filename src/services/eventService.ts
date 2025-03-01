6b10660ed376d0f40652036ecc20b3c679e91572
chalermsak-s <chalermsak-s@cmu.ac.th>
2025-03-01 09:52:05 +0700

    Lab5 Task1.1-1.8

---
+++ src/services/eventService.ts
@@ -0,0 +1,22 @@
+import type { Event } from '../models/event'
+import * as repo from '../repository/eventRepositoryPrisma'
+
+export function getEventByCategory(category: string) {
+  return repo.getEventByCategory(category)
+}
+
+export function getAllEvents() {
+  return repo.getAllEventsWithOrganizer()
+}
+
+export function getEventById(id: number) {
+  return repo.getEventById(id)
+}
+
+export function addEvent(newEvent: Event) {
+  return repo.addEvent(newEvent)
+}
+
+export function getAllEventsWithPagination(pageSize: number, pageNo: number) {
+  return repo.getAllEventsWithOrganizerPagination(pageSize, pageNo)
+}
